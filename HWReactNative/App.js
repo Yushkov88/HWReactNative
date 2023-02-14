78ba2d09730d4c93625ed4328faf1d24bdf864ab
Yushkov Vitaliy <98348805+Yushkov88@users.noreply.github.com>
2023-02-14 20:07:27 +0200

    move files&change names

--- HWReactNative/App.js
+++ HWReactNative/App.js
@@ -1,17 +1,16 @@
 import { useState, useCallback } from "react";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
 import { useFonts } from "expo-font/build/FontHooks";
 import * as SplashScreen from "expo-splash-screen";
 import { StyleSheet, View } from "react-native";
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState("Registration");
+  const [currentScreen, setCurrentScreen] = useState("Registration");
 
   const [fontsLoaded] = useFonts({
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-    // "Circe-Regular": require("./assets/fonts/Circe-Regular.ttf"),
   });
 
   const onLayoutRootView = useCallback(async () => {
@@ -26,10 +25,10 @@ export default function App() {
 
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
-      {currentPage === "Registration" ? (
-        <RegisterPage changePage={setCurrentPage} />
+      {currentScreen === "Registration" ? (
+        <RegisterScreen changePage={setCurrentScreen} />
       ) : (
-        <LoginPage changePage={setCurrentPage} />
+        <LoginScreen changePage={setCurrentScreen} />
       )}
     </View>
   );
